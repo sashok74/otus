9fe2ed64b742bc7e5329126f8229ce03e403c5d8
sashok74 <rolliks@gmail.com>
2025-01-06 18:41:53 +0000

    Deploying to gh-pages from  @ 041258372383ec242cd073b49549bcc5354d80bb 🚀

--- search/all_4.js
+++ search/all_4.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['getdocument_0',['getDocument',['../classEditor.html#a7aebfa4dcd2d178abc116f3eadca8dd0',1,'Editor']]],
-  ['getprimitives_1',['getPrimitives',['../classDocument.html#aae205382ebae6cf263b8f40247e3a59b',1,'Document']]],
+  ['getprimitives_1',['getPrimitives',['../classDocument.html#a5296a5e3b3d63c2ecced2564484aa947',1,'Document']]],
   ['graphicprimitive_2',['GraphicPrimitive',['../classGraphicPrimitive.html',1,'']]]
 ];
